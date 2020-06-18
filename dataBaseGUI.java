fe9b52450fd9b3b15ec0590d9cde2d088fafd6bd
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-18 17:06:19 -0400

    Going to try and figure out how to use a combobox

--- dataBaseGUI.java
+++ dataBaseGUI.java
@@ -1,7 +1,24 @@
 import javax.swing.*; 
 
+
 public class dataBaseGUI extends JFrame{
     private static final long serialVersionUID = 1L;
+
+    JPanel panel = new JPanel(); 
+
+    JLabel nameLabel = new JLabel("Enter your name"); 
+    JLabel birthdayLabel = new JLabel("Enter your birthday"); 
+    JLabel heightLabel = new JLabel("Enter your height"); 
+    JLabel weightLabel = new JLabel(""); 
+
+    JTextField name = new JTextField(); 
+    JTextField birthday = new JTextField(); 
+    JTextField height = new JTextField(); 
+    JTextField weight = new JTextField(); 
+
+    JButton submit = new JButton("Submit"); 
+    
+
 
     public dataBaseGUI(){
         setSize(400,400); 
