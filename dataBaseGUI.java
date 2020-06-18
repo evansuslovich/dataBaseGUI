bf40ad5d034edd904e3c08f60a6122441fb6f123
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-18 16:51:00 -0400

    I plan on making a database with a GUI

---
+++ dataBaseGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*; 
+
+public class dataBaseGUI extends JFrame{
+    private static final long serialVersionUID = 1L;
+
+    public dataBaseGUI(){
+        setSize(400,400); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        
+        setVisible(true); 
+    }
+    public static void main(String[] args) {
+        new dataBaseGUI(); 
+        
+    }
+}
