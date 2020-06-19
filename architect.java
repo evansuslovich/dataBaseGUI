8a1b6ea709d4a6318a9b4f145f8823822483dec4
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-19 15:02:20 -0400

    architect game is going to allow the user to create a blueprint of his/her house .. pool .. trees .. backyard .. through a birds eye view .. I believe there is going to have to be a lot of programming with arraylists

---
+++ architect.java
@@ -0,0 +1,18 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+
+
+public class architect extends JFrame implements ActionListener{
+
+    public architect(){
+
+        
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
