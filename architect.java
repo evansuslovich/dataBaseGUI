6a53a63d3d82d2f28751653823fff1ec7256be49
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-20 08:08:46 -0400

    I am going to start of by making the game a tree placer. I will have to create a class called trees that will  be a jlabel or icon. Everywhere the user clicks, a tree will appear.

--- architect.java
+++ architect.java
@@ -4,10 +4,25 @@ import java.awt.event.*;
 
 
 public class architect extends JFrame implements ActionListener{
+    private static final long serialVersionUID = 1L;
+
+
+    JPanel menu = new JPanel(); 
+    
+
+
 
     public architect(){
+        setTitle("Architect Game");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,800); 
 
-        
+
+
+        setVisible(true); 
+
+        // open new frames to design new things 
+        // tree placer 
 
     }
 
